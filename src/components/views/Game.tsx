aa3fb5f64fd8acd47df17219807f53389c31e2bc
Hanky Liu <103504158+zihanltesla@users.noreply.github.com>
2024-02-24 13:28:06 +0100

    Update Game.tsx

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -56,9 +56,18 @@ const Game = () => {
   }, [navigate]); // Correct dependency
 
   const logout = async () => {
-    // Assuming logout logic is correctly implemented on your backend
-    localStorage.removeItem("token");
-    navigate("/login");
+    const id = localStorage.getItem('id');
+    localStorage.removeItem('token');
+    try {
+      const requestBody = JSON.stringify({id:id});
+      const response = await api.post('/users/logout', requestBody);
+      console.log(response);
+    
+    } catch (error) {
+      alert(`Something went wrong during the logout: \n${handleError(error)}`);
+      
+    }
+    navigate('/login');
   };
 
   let content = users.length > 0 ? (
